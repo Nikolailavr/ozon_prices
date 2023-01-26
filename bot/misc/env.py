e552a0531dea69176348012aa6d1ef7fe5d00ab6
Nikolai <hukolaulos@gmail.com>
2023-01-26 11:26:29 +0700

    delete adminID

--- bot/misc/env.py
+++ bot/misc/env.py
@@ -4,4 +4,4 @@ from typing import Final
 
 class TgKeys:
     TOKEN: Final = environ.get('OzonToken', 'define me!')
-    admin_chatID = 161622650
+    admin_chatID = environ.get('AdminID', 'define me!')
