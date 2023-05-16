c502888937416c2353217ec1ba1015e9168681a1
Nikolai <hukolaulos@gmail.com>
2023-05-16 12:40:52 +0700

    v1.02 Fix search price in html

--- bot/db/tables/prices.py
+++ bot/db/tables/prices.py
@@ -9,13 +9,17 @@ async def update_price(link: Link) -> None:
         INSERT OR REPLACE INTO prices
         (url_link, price, price_ozon)
         VALUES (:url_link, :price, :price_ozon);"""
-    async with aiosqlite.connect(config.SQLITE_DB_FILE) as db:
-        await db.execute(
-            sql,
-            {
-                "url_link": link.url,
-                "price": link.price,
-                "price_ozon": link.price_ozon,
-            },
-        )
-        await db.commit()
+    try:
+        async with aiosqlite.connect(config.SQLITE_DB_FILE) as db:
+            await db.execute(
+                sql,
+                {
+                    "url_link": link.url,
+                    "price": link.price,
+                    "price_ozon": link.price_ozon,
+                },
+            )
+            await db.commit()
+    except Exception as ex:
+        raise ex
+
--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from aiogram import Bot
-import time
+import asyncio
 
 from bot.misc import TgKeys, logger, config
 from bot.db import read_links, update_price
@@ -9,15 +9,17 @@ from bot.db.main import Link
 
 
 async def start_checking() -> None:
-    links = await read_links()
-    for link in links:
-        await _checking(link=link)
-
-
-async def _checking(link: Link) -> None:
     bot = Bot(token=TgKeys.TOKEN, parse_mode='HTML')
+    links = await read_links()
+    tasks = [_checking(link=link, bot=bot) for link in links]
+    await asyncio.gather(*tasks)
+    session = await bot.get_session()
+    await session.close()
+
+
+async def _checking(link: Link, bot: Bot) -> None:
     html = ""
-    title = "Just a moment..."
+    title = ""
     service = Service(executable_path=config.driver_path)
     try:
         driver = Chrome(service=service, options=config.options)
@@ -43,6 +45,7 @@ async def _checking(link: Link) -> None:
         price_dict = await _check_price(html=html, title=title, link=link)
         if price_dict['msg_need']:
             await bot.send_message(chat_id=link.id, text=price_dict['text'])
+        print(price_dict['result'])  # Вывод в консоль результата поиска
 
 
 async def _check_price(html: str, title: str, link: Link) -> dict:
@@ -54,7 +57,7 @@ async def _check_price(html: str, title: str, link: Link) -> dict:
     text = ''
     price = _find_price(html, config.find_price)
     price_ozon = _find_price(html, config.find_price_ozon)
-    print(f'{title} | Цена: {price} р, Цена (Ozon): {price_ozon} р')  # Для отображения работы бота
+    result = f'{title} | Цена: {price} р, Цена (Ozon): {price_ozon} р'  # Для отображения работы бота
     msg_need = False
     if price != link.price or price_ozon != link.price_ozon:
         text = f"{title}\n"
@@ -67,8 +70,8 @@ async def _check_price(html: str, title: str, link: Link) -> dict:
         text += f"{link.url}"
         link.price = price
         link.price_ozon = price_ozon
-        await update_price(link=link)
-    return {'msg_need': msg_need, 'text': text}
+        await update_price(link)
+    return {'msg_need': msg_need, 'text': text, 'result': result}
 
 
 def _find_price(html: str, find_text: tuple) -> int:
