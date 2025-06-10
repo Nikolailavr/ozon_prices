2adbf17867da63df4e0d57685eb0d8f6e962049e
Nikolai Lavrov <mail@nlavrov.ru>
2025-06-10 13:25:32 +0700

    v2.01_009demo: Update check price after add

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 docker volume prune -f
-docker compose --env-file ./src/.env up -d --build redis pg flower
+docker compose --env-file ./src/.env up -d --build
+#docker compose --env-file ./src/.env up -d --build redis pg flower
 #docker compose --env-file ./src/.env up -d --build app
-#docker compose --env-file ./src/.env up --build
+#docker compose --env-file ./src/.env up -d --build celery-1
 docker image prune -f
-cd src
-poetry run celery -A apps.celery.celery_app worker --pool=threads --loglevel=info -c 1
+#cd src
+#poetry run celery -A apps.celery.celery_app worker --pool=threads --loglevel=info -c 1
