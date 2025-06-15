d254f0850230172641491cefaf2d17eca8469712
Nikolai Lavrov <mail@nlavrov.ru>
2025-06-15 19:04:11 +0700

    v2.01_009demo: Update check price after add

---
+++ src/runcelery.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Запуск виртуального дисплея Xvfb..."
+Xvfb :99 -screen 0 1920x1080x24 &
+
+echo "Запуск оконного менеджера fluxbox..."
+fluxbox &
+
+echo "Запуск VNC-сервера..."
+x11vnc -display :99 -forever -passwd 123456 -shared -nopw &
+
+echo "Ожидание запуска Xvfb и VNC..."
+sleep 5
+
+echo "Запуск Celery worker..."
+exec celery -A apps.celery.celery_app worker --pool=threads --loglevel=info -c 1
